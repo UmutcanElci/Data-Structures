6adfb6d359541a01c07a4a9ff648c4f2fa83566d
Umutcan Elci <umutcanelci@yaani.com>
2022-08-28 21:07:44 +0300

    String Classes

--- src/UsefulClasses/StringClass.java
+++ src/UsefulClasses/StringClass.java
@@ -1,6 +1,13 @@
 package UsefulClasses;
 import java.lang.String;
 public class StringClass {
-//Also String builder
+//Important Constructors of StringBuilder class
+    //StringBuilder() It creates an empty String builder object also we can create StringBuilder(String str or int lenght) specific string or int
 
+    //1) StringBuilder append() Method
+    //
+    //
+    //
+    //
+    //
 }
