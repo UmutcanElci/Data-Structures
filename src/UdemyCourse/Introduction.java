40bee0a8ca69b157cde10e352248bd162ece3358
Umutcan Elci <umutcanelci@yaani.com>
2022-08-29 15:54:05 +0300

    UdemyCourse

---
+++ src/UdemyCourse/Introduction.java
@@ -0,0 +1,61 @@
+package UdemyCourse;
+
+public class Introduction {
+    //What is data Structure
+    /*
+    Data Structures are different ways of organizing data on your computer, that can be used effectively
+    Verileri bir bakıma düzenlemek diyebiliriz düzenli olan veriler arasında gezmek ve istediğimiz veriyi çekmek daha kolay basit Veri yapıları anlamı
+    zorlaşacağı yer tiplerinde.
+    Data = Book
+    Data Structures = Library
+    Algorithm = find book
+     */
+    //What is algorithm
+    /*
+    Algorithms are set of instructions to perform a task or action
+    Düzenlenmiş olan verilerimizi işlemek için algoritmaları kullanırız.Günlük yaşantımızda ki yaptığımız herşey algoritmaya örnek verilebilir. Mesela işe gitmek
+    önce otobüse bineriz kahve alırız işe yürürüz sonra işe varırız. Peki burdaki verilerimiz nedir? Mesela adress olabilir otobüs durağının ya da kahve aldığımız yerin
+    kahve seçeneklerimiz de bir veridir. Veriler her yerde algoritmalar verileri anlamlı kılıyor. Kısaca veri yapıları veileri düzenler algoritmalara düzenli verileri
+    işler.
+     */
+    //Types of Data Structures
+    /*
+    We can grouped into two basic types
+    1) Primitive Data Structures
+    -Integer
+    -Float
+    -Character
+    -Boolean
+    2) Non Primitive Data Structures
+    This data structures can divide to two
+    *Linear Data Structures
+    In Linear Data Structures items are arranged in memory, in linear sequential manner, also they can be static or dynamic.
+    Linear veri yapıları bellekte doğrusal sıralı olarak düzenlenir ve static veya dinamik olabilirler.
+    Peki statik ve dinamik veri yapısı ne demek?
+    Statik veri yapıları sabit sayıda veri öğesi depolaması yapar önceden gelen verilerin boyutu bellidir ve ona göre bir yer bırakılır.
+    Dinamik veri yapılarında veri sayısını önceden bilmeyiz, bu da bellek sabit olamdığından gelen verinin bellek sınırını taşma olasılığı veya
+    veri için açılmış olan belleğin boş olması ile sonuçlanır. Bunun içni yazılımcı verileri ve belleği kontrol etmelidir.
+    1-Static
+    -Array
+    In static linear data structures sizes and structures associated with memory locations are fixed at a compiled time.
+    2-Dynamic
+    -Linked list
+    -Stack
+    -Queue
+    In dynamic data structures associated memory locations changed.
+    *Non Linear Data Structures
+    -Tree
+    -Graph
+    In non linear Data Structures data item can be connected with more than one data items
+     */
+    //Types of Algorithms
+    /*
+    -Simple recursive algorithms
+    -Divide and conquer algorithms
+    -Dynamic programming algorithms
+    -Greedy algorithms
+    -Brute force algorithms
+    -Randomized algorithms
+     */
+
+}
---
+++ src/UdemyCourse/Recursion.java
@@ -0,0 +1,30 @@
+package UdemyCourse;
+
+public class Recursion {
+    //What is Recursion?
+    /*
+    Recursion: A way of solving a problem by having a function calling itself.
+    Recursion yenileme demek ve preblemimizi aynı fonksiyonu tekrar tekrar çağırarak küçük hale getirmek diyebiliriz. Aynı kodu çalıştırırız kontrol ederiz yoksa
+    bir eksiltir tekrar çalıştırırız. Örnek Matruşka bebek oyuncağı olabilir:
+     */
+    public static void openRussianDoll(int dollNum){
+        if(dollNum == 1){
+            System.out.println("All dolls are opened");
+        }
+        else {
+            openRussianDoll(dollNum-1);
+        }
+    }
+
+    //Why/Where we need recursion ?
+    /*
+    1) Recursion thinking is really important in programming, and it helps you break down big problems into smaller ones and easier to use
+    -When to choose recursion?
+    *If you can divide the problem into similar sub problems
+    *Design an algorithm to compute nth...
+    *Write code to list the n...
+    *Implement a method to compute all
+    2)The prominent usage of recursion in data structures like tree and graphs
+    3)It is used in many algorithms (Divide and conquer,greedy,dynamic)
+     */
+}
