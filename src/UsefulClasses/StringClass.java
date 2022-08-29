814b48d9fb8376b2d6aeeaa7d83db36fd9a6f3db
Umutcan Elci <umutcanelci@yaani.com>
2022-08-29 15:52:47 +0300

    String

--- src/UsefulClasses/StringClass.java
+++ src/UsefulClasses/StringClass.java
@@ -5,9 +5,10 @@ public class StringClass {
     //StringBuilder() It creates an empty String builder object also we can create StringBuilder(String str or int lenght) specific string or int
 
     //1) StringBuilder append() Method
-    //
-    //
-    //
-    //
-    //
+    //2) StringBuilder insert() Method
+    //3) StringBuilder replace() Method
+    //4) StringBuilder delete() Method
+    //5) StringBuilder reverse() Method
+    //6) StringBuilder capacity() Method
+    //7) StringBuilder ensureCapacity() Method
 }
