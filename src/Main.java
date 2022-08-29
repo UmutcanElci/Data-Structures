1017e4037ad1f7f342782e18fd4d7015a4b04f7d
Umutcan Elci <umutcanelci@yaani.com>
2022-08-29 15:53:40 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         //This project is learn to Data Structures
+        //https://www.cs.usfca.edu/~galles/visualization/Algorithms.html
 
     }
 }
