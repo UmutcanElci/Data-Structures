b7ee70776261dfeffa43b166b8fffd86508db0f1
Umutcan Elci <umutcanelci@yaani.com>
2022-08-31 15:11:43 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 
-
+import UdemyCourse.Recursion;
 public class Main {
     public static void main(String[] args) {
         //This project is learn to Data Structures
         //https://www.cs.usfca.edu/~galles/visualization/Algorithms.html
-
+        Recursion rec = new Recursion();
+        var result = rec.decimalToBinary(13);
+        System.out.println(result);
     }
 }
