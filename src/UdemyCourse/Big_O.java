b5fcc211f5e2bc2aa7a7625f4399ad621eac1ba1
Umutcan Elci <umutcanelci@yaani.com>
2022-09-03 22:59:08 +0300

    Big-O

--- src/UdemyCourse/Big_O.java
+++ src/UdemyCourse/Big_O.java
@@ -72,8 +72,117 @@ public class Big_O {
     How to measure R    ecursive Algorithm ?
     How to measure Recursive Algorithm with multiple calls ?
     --NOTİON!
+     */
 
+    /*
+     Time Complexity Questions :
 
+    Question 1 - Time Complexity of Method that returns Sum and Product of Array(It's about the time complexity not the question)
+    - Create a function which calculates the sum and product of elements of array
+    - Find the time complexity for created method
 
      */
+    //Now time complexity
+    public void sumAndProductArr(int[] array){
+        int sum = 0;// O(1)
+        int product = 1;//O(1)
+        for(int i  = 0; i<array.length; i++){//O(n)
+            sum += array[i];//O(1)
+        }
+        for (int i = 0 ; i<array.length; i++){//O(n)
+            product *= array[i];//O(1)
+        }
+        System.out.println(sum + " , " + product);//O(1)
+    }
+    //If we combine this according to our rule we can eliminate non - dominant terms . Non - deominant terms are O(1)s.
+    //And if we sum this O(n) we will get two O(n)
+    //So the time complexity is O(n)
+
+    /*
+    Question 2 - Time Complexity of Print Pairs Method
+    - Create a function which prints to the console the pairs from given array.
+    - Find the time complexity for created method.
+    */
+    public void printPairsArray(int[] array){
+
+        for (int i = 0; i<array.length; i++ ){// O(n)
+            for(int j = 0 ; j<array.length; j++){// O(n) so this two complexity's are multiple so Time Complexity is O(n^2)
+                System.out.print(array[i] +" ," + array[j]);// O(1)
+            }
+            System.out.println();//O(1)
+        }
+    }
+    //Time Complexity O(n^2)
+
+    /*
+    Question 3 - Time Complexity of Print Unordered Pairs Method
+    - Find the time complexity of this method
+     */
+    public void printUnorderedPairs(int[] array){
+        for(int i = 0; i<array.length;i++){
+            for (int j =i+1; j<array.length; j++){
+                System.out.println(array[i] + " ," + array[j]);
+            }
+        }// Like if array is {1,3,4,5} the output is 13,14,15 than 34,35 than 45 because of the j = i+1
+    }
+    //In this course we talked about how we understand the time complexity
+    /*
+    Question 4 - Find Time Complexity for given method
+     */
+    public void printUnorderedPairs(int[] arrayA , int[] arrayB){
+        for(int i = 0 ; i<arrayA.length;i++){//Well first thought maybe is this code time complexity is O(N^2) but wrong cause that's two different arrays.
+            for (int j = 0 ; j<arrayB.length;j++){//That's why time complexity completely different. So in this case for each element for A, we are looping through all elements of the BSo in this case the time complexity will be O(ab)
+                if(arrayA[i] < arrayB[j]){//O(1)
+                    System.out.println(arrayA[i] + " , "+arrayB[j]);//O(1)
+                }
+            }
+        }
+    }
+    /*
+
+    Question 5 - Find Time Complexity for given Method
+     */
+    public void printUnorderedParis2(int[] arrayA, int[] arrayB){
+        for(int i = 0 ; i<arrayA.length;i++){
+            for (int j = 0 ; j<arrayB.length;j++){//We know this part takes O(ab) time complexity
+                for(int k = 0; k<100000;k++){//O(1) number is constant
+                    System.out.println(arrayA[i] + " , "+arrayB[j]);//O(1)
+                }
+            }
+        }
+    }
+    /*
+    Question 6 - Time Complexity of Reverse Array Function
+     */
+    public void reverse(int[] array){
+        for(int i = 0; i<array.length/2;i++){//for example when i = 0 and array length is 4. So time complexity is O(n/2)
+            int other = array.length-i-1;//other = 3 time complexity is O(1)
+            int temp = array[i];// = temp = 0; time complexity is O(1)
+            array[i] = array[other];//[0] = [3] //values changed time complexity O(1)
+            array[other] = temp;// [3] = [0] time complexity is O(1)
+        }
+    }
+    /*
+    Question 7 - Equivalent to O(N)
+    - Which of the following are equivalent to O(N)?
+    1) O(N+P), where P<N/2 - Because of the P is non-dominant we can delete and write this time complexity O(N)
+    2) O(2N) - 2 is the constant (non-dominant) se we can delete it and the time complexity is O(N)
+    3) O(N+logN) - logN we know is the constant also so time complexity is O(N)
+    4) O(N+NlogN) - In this case O(N) the non-dominant so time complexity is O(NlogN)
+    5) O(N+M) -
+     */
+
+    /*
+    Question 8 - Time Complexity of Factorial
+     */
+    public static int factorial(int n){//O(n)
+        if(n < 0){//O(n)
+            return -1;
+        } else if (n == 0) {
+            return 1;
+        }else {//O(n-1)
+            return n*factorial(n-1);
+        }
+    }
+    
 }
