b7ea12196a481c42abcd82c25c5650e2f69fed09
Umutcan Elci <umutcanelci@yaani.com>
2022-09-11 13:06:22 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 
+import UdemyCourse.Big_O;
 import UdemyCourse.Recursion;
 public class Main {
     public static void main(String[] args) {
         //This project is learn to Data Structures
         //https://www.cs.usfca.edu/~galles/visualization/Algorithms.html
-        Recursion rec = new Recursion();
-        var result = rec.decimalToBinary(13);
-        System.out.println(result);
+        int[] arr = {1,5,3,7,2};
+        Big_O bigO = new Big_O();
+        bigO.printPairsArray(arr);
     }
 }
