adfcc7c81f0b4cf933be57b489152655116e72be
Umutcan Elçi <umutcanelci@yaani.com>
2022-10-02 10:19:21 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 
 import UdemyCourse.Big_O;
 import UdemyCourse.Recursion;
+import Questions.ArrayQuestions;
 public class Main {
     public static void main(String[] args) {
         //This project is learn to Data Structures
         //https://www.cs.usfca.edu/~galles/visualization/Algorithms.html
-        int[] arr = {1,5,3,7,2};
-        Big_O bigO = new Big_O();
-        bigO.printPairsArray(arr);
+        ArrayQuestions  test = new ArrayQuestions();
+        int intArray[] = {10,20,30,50,40};
+        int intArray2[] = {20,10,30,50,40};
+        //String pairs = test.maxProduct(intArray);
+        //System.out.println(pairs);
+        //boolean equal = test.isUnique(intArray);
+        //System.out.println(equal);
+
+        boolean ts = test.permutation(intArray,intArray2);
+        System.out.println(ts);
     }
 }
